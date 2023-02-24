d967407f77ca89b0100929375c92dcea89f6ac1e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-24 10:06:20 +0100

    feat: build the fetch news function 1/2. add graphql request library

---
+++ lib/fetchNews.ts
@@ -0,0 +1,12 @@
+const fetchNews = async (
+  category?: Category | string,
+  keywors?: string,
+  isDynamic?: boolean
+) => {
+  // GraphQL query
+  // Fetch function with Next.js 13 catching
+  // Sort function by images vs not images present
+  // Return sorted array
+};
+
+export default fetchNews;
--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,28 @@
+type Article = {
+  author: strung | null;
+  categoty: string;
+  country: string;
+  description: string;
+  image: string | null;
+  language: string;
+  published_at: string;
+  source: string;
+  title: string;
+  url: string;
+};
+
+type Pagination = {
+  count: Int;
+  limit: Int;
+  offset: Int;
+  total: Int;
+};
+
+type NewsResponse = {
+  pagination: Pagination;
+  data: Article[];
+};
+
 type Category =
   | "business"
   | "entertainment"
