a479ace81c19751a97d9aab841d9129748490e2b
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-24 19:05:39 +0100

    feat: install line clamp for tailwind css

--- app/Article.tsx
+++ app/Article.tsx
@@ -18,7 +18,7 @@ function Article({ article }: Props) {
           <h2 className="font-bold font-serif">{article.title}</h2>
 
           <section className="mt-2 flex-1">
-            <p className="text-xs line-clamp-6">{article.description}</p>
+            <p className="text-xs line-clamp-2">{article.description}</p>
           </section>
 
           <footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-400">
--- app/NewsList.tsx
+++ app/NewsList.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function NewsList({ news }: Props) {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p10 gap-10">
+    <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-10 gap-10">
       {news.data.map((article) => (
         <Article key={article.title} article={article} />
       ))}
