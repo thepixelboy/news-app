17094fc16758e8a8cc4ca16f065806c6f99331cc
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-24 10:35:13 +0100

    feat: implement mediastack api

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
-function Homepage() {
-  return <div>Homepage</div>;
+import { categories } from "../constants";
+import fetchNews from "../lib/fetchNews";
+
+async function Homepage() {
+  // fetch the news data
+  // const news: NewsResponse = await fetchNews(categories.join(','));
+
+  return <div>{/* News List */}</div>;
 }
 export default Homepage;
