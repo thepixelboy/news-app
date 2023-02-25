964a0db08b011191668549f2c3830fb1ea86c481
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-25 14:19:08 +0100

    feat: add dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,12 @@ export default function RootLayout({
   return (
     <html>
       <head />
-      <Providers>
-        <body className="bg-gray-100 dark:bg-zinc-900 transition-all duration-700">
+      <body className="bg-gray-100 dark:bg-zinc-900 transition-all duration-700">
+        <Providers>
           <Header />
           <div className="max-w-6xl mx-auto">{children}</div>
-        </body>
-      </Providers>
+        </Providers>
+      </body>
     </html>
   );
 }
