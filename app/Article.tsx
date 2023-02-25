8cbdc31f4014b77a41a47f7e6b719d4808e981c3
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-25 14:31:10 +0100

    feat: read more button

--- app/Article.tsx
+++ app/Article.tsx
@@ -1,3 +1,5 @@
+import ReadMoreButton from "./ReadMoreButton";
+
 type Props = {
   article: Article;
 };
@@ -27,7 +29,7 @@ function Article({ article }: Props) {
           </footer>
         </div>
 
-        {/* read more button */}
+        <ReadMoreButton article={article} />
       </div>
     </article>
   );
