325f8b41e89f8e575a2d81f756455057ed60ea7b
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-25 15:50:40 +0100

    feat: loading state

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+function Loading() {
+  return (
+    <div className="animate-pulse font-serif text-lg text-gray-400 text-center p-10">
+      Loading News feed...
+    </div>
+  );
+}
+export default Loading;
